542ad1fc90e86a985863f2e02117f5b1b9f95088
Ryusei <yr.980827@gmail.com>
2025-08-08 16:01:49 +0900

    ログイン機能を追加

--- routes/auth.js
+++ routes/auth.js
@@ -15,6 +15,20 @@ router.post("/register", async (req, res) => {
   }
 });
 
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(404).send("ユーザが見つかりません");
+    const validPassword = req.body.password === user.password;
+    if (!validPassword) return res.status(400).send("パスワードが違います");
+    return res.status(200).json(user);
+  } catch (err) {
+    return res.status(500).json({
+      errorMessage: err,
+    });
+  }
+});
+
 // router.get("/", (req, res) => {
 //   res.send("auth router");
 // });
